3b4371c2f2b985bec22d2d4b316ef7d15bf39ca8
DanielGlazman <103389828+DanielGlazman@users.noreply.github.com>
2022-12-30 14:59:44 +0200

    Entities of Customer, Employee, ParkingLotWorker and Membership - Not Final!

---
+++ entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/ParkingLotWorker.java
@@ -0,0 +1,33 @@
+package il.cshaifasweng.OCSFMediatorExample.entities;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Parking Lot Workers")
+
+public class ParkingLotWorker extends Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private int parkingLotNumber;
+
+
+    public ParkingLotWorker(int employeeId, String firstName, String lastName, String email, String workersRole, int parkingLotNumber)
+    {
+        super(employeeId, firstName, lastName, email, workersRole);
+        this.parkingLotNumber = parkingLotNumber;
+    }
+
+    public ParkingLotWorker() {}
+
+    public int getParkingLotNumber() {
+        return parkingLotNumber;
+    }
+    public void setParkingLotNumber(int parkingLotNumber) {
+        this.parkingLotNumber = parkingLotNumber;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+}
