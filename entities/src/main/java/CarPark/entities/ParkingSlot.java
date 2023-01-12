ab6a860986d1212e0f8c2e1d2d6289015e9553e8
noy166 <noybl101@gmail.com>
2023-01-12 16:38:54 +0200

    Edit parkingSlots new format

---
+++ entities/src/main/java/CarPark/entities/ParkingSlot.java
@@ -0,0 +1,67 @@
+package CarPark.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+
+@Entity
+@Table(name = "parkingslots")
+public class ParkingSlot implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private Status SpotStatus = Status.EMPTY;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parkinglot_id")
+    private Parkinglot parkinglot;
+
+    @OneToOne
+    private CheckedIn checkedIn;
+
+    public CheckedIn getCheckedIn() {
+        return checkedIn;
+    }
+
+    public void setCheckedIn(CheckedIn checkedIn) {
+        this.checkedIn = checkedIn;
+    }
+
+    public ParkingSlot() {
+    }
+
+    public ParkingSlot(Parkinglot parkinglot) {
+        setParkinglot(parkinglot);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Status getSpotStatus() {
+        return SpotStatus;
+    }
+
+    public void setSpotStatus(Status spotStatus) {
+        SpotStatus = spotStatus;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Parkinglot getParkinglot() {
+        return parkinglot;
+    }
+
+    public void setParkinglot(Parkinglot parkinglot) {
+        this.parkinglot = parkinglot;
+        parkinglot.getParkingSlots().add(this);
+    }
+    public Boolean getStatus() {
+        return SpotStatus==Status.EMPTY;
+    }
+    public enum Status {EMPTY, USED, RESTRICTED, RESERVED}
+}
---
+++ entities/src/main/java/CarPark/entities/messages/ParkingSlotsMessage.java
@@ -0,0 +1,43 @@
+package CarPark.entities.messages;
+import CarPark.entities.ParkingSlot;
+import CarPark.entities.Parkinglot;
+//import javafx.collections.ObservableList;
+import java.util.List;
+
+public class ParkingSlotsMessage extends Message {
+    public RequestType request_type;
+    public ResponseType response_type;
+    public List<Parkinglot> parkingList;
+    public String parkinglotId;
+    //public ObservableList<ParkingSlot> parkingslots;
+    public List<ParkingSlot> parkingSlots;
+
+    public ParkingSlotsMessage(MessageType message_type, ParkingSlotsMessage.RequestType requestType) {
+        super(message_type);
+        this.request_type = requestType;
+    }
+    public ParkingSlotsMessage(MessageType message_type, ParkingSlotsMessage.RequestType request_type, String parkinglotId) {
+        super(message_type);
+        this.request_type = request_type;
+        this.parkinglotId = parkinglotId;
+    }
+    public ParkingSlotsMessage(MessageType message_type, ParkingSlotsMessage.ResponseType response_type, List<ParkingSlot> parkingSlots) {
+        super(message_type);
+        this.response_type = response_type;
+        this.parkingSlots = parkingSlots;
+    }
+
+
+    public enum RequestType {
+        GET_ALL_PARKING_LOTS,
+        GET_SELECTED_PARKING_SLOTS,
+        SET_PARKING_SLOT_STATUS
+    }
+
+    public enum ResponseType {
+        SET_ALL_PARKING_LOTS,
+        SET_PARKING_SLOTS,
+        PARKING_SLOT_EDITED
+    }
+
+}
