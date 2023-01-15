5935e576cbc15180001d6924dc9dd0be6032571b
noy166 <noybl101@gmail.com>
2023-01-15 19:49:52 +0200

    create manager + CEO reports - temporary

--- server/src/main/java/CarPark/server/handlers/OrderHandler.java
+++ server/src/main/java/CarPark/server/handlers/OrderHandler.java
@@ -2,7 +2,7 @@ package CarPark.server.handlers;
 
 import CarPark.entities.Order;
 import CarPark.entities.messages.Message;
-import CarPark.entities.messages.OrderMessage;
+import CarPark.entities.messages.CreateOrderMessage;
 import CarPark.server.ocsf.ConnectionToClient;
 import org.hibernate.Session;
 
@@ -11,11 +11,11 @@ import java.util.List;
 
 public class OrderHandler extends MessageHandler {
 
-    private final OrderMessage class_message;
+    private final CreateOrderMessage class_message;
 
     public OrderHandler(Message msg, Session session, ConnectionToClient client) {
         super(msg, session, client);
-        this.class_message = (OrderMessage) this.message;
+        this.class_message = (CreateOrderMessage) this.message;
     }
 
     @Override
@@ -23,7 +23,7 @@ public class OrderHandler extends MessageHandler {
         switch (class_message.request_type) {
             case CREATE_NEW_ORDER:
                 createOrder();
-                class_message.response_type = OrderMessage.ResponseType.ORDER_SUBMITTED;
+                class_message.response_type = CreateOrderMessage.ResponseType.ORDER_SUBMITTED;
                 break;
         }
     }
