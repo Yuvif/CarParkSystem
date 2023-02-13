5593276384c39f848bf7dcaa147c3fad0a6ab61e
Yuval <100403800+Yuvif@users.noreply.github.com>
2023-02-13 20:27:16 +0200

    fix real time updates

--- client/src/main/java/CarPark/client/controllers/Employee/CEO/CEOPricesController.java
+++ client/src/main/java/CarPark/client/controllers/Employee/CEO/CEOPricesController.java
@@ -32,7 +32,7 @@ public class CEOPricesController {
     @FXML
     private TableColumn<Price, Integer> new_pricesCol;
     @FXML
-    private TableColumn<Price,String> plCol;
+    private TableColumn<Price, String> plCol;
 
     @FXML
     private TableView<Price> pricesTable;
@@ -40,7 +40,7 @@ public class CEOPricesController {
     private TableColumn<Price, Void> approveCol;
 
     @FXML
-    void initialize () throws IOException{
+    void initialize() throws IOException {
         EventBus.getDefault().register(this);
         typeCol.setCellValueFactory(new PropertyValueFactory<>("parkingType"));
         pricesCol.setCellValueFactory(new PropertyValueFactory<>("price"));
@@ -102,100 +102,16 @@ public class CEOPricesController {
 
     @Subscribe
     public void newResponse(PricesMessage new_message) throws IOException {
-        switch (new_message.response_type)
-        {
+        switch (new_message.response_type) {
             case SET_REQUESTS_TABLE:
                 pricesTable.setItems(FXCollections.observableArrayList(new_message.priceList));
                 break;
-            case WAITING_FOR_APPROVAL:
-            {
-                sendAlert("New change request received."+ "\n" +"From:" + new_message.parkingLot,
+            case WAITING_FOR_APPROVAL: {
+                sendAlert("New change request received." + "\n" + "From:" + new_message.parkingLot,
                         "New Request", Alert.AlertType.INFORMATION);
                 PricesMessage msg = new PricesMessage(Message.MessageType.REQUEST, PricesMessage.RequestType.GET_REQUESTS_TABLE);
                 SimpleClient.getClient().sendToServer(msg);
-                break;
             }
         }
-    }
-
-    public static class PricesManagerController {
-        @FXML // fx:id="plPick"
-        private Label parking_lot;
-        @FXML
-        private TableView<Price> pricesTable;
-        @FXML
-        private TableColumn<Price, String> typeCol;
-        @FXML
-        private TableColumn<Price, String> paymentCol;
-        @FXML
-        private TableColumn<Price, Integer> pricesCol;
-        @FXML
-        private TableColumn<Price, Integer> noCol;
-        @FXML
-        private TableColumn<Price, Integer> hopCol;
-
-
-        @FXML
-        private void loginPage() throws IOException {
-            SimpleChatClient.setRoot("ParkingLotManagerPage");
-        }
-
-
-        @FXML
-        private void Edit() {
-            //case editing price
-            pricesTable.setEditable(true);
-            pricesCol.setCellFactory(TextFieldTableCell.<Price, Integer>forTableColumn(new IntegerStringConverter()));
-            pricesCol.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<Price, Integer>>() {
-                @Override
-                public void handle(TableColumn.CellEditEvent<Price, Integer> priceIntegerCellEditEvent) {//send request to server to set new price on DB
-                    Price new_price = priceIntegerCellEditEvent.getRowValue();
-                    new_price.setPrice(priceIntegerCellEditEvent.getNewValue());
-                    PricesMessage message = new PricesMessage(Message.MessageType.REQUEST, PricesMessage.RequestType.EDIT_PRICE, new_price);
-
-                    ParkingLotWorker current_manager = (ParkingLotWorker) SimpleClient.getCurrent_user();
-                    message.parkingLot = current_manager.getParkinglot().getName();
-                    try {
-                        SimpleClient.getClient().sendToServer(message);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-        }
-
-
-        @FXML
-        void initialize() throws IOException {
-            EventBus.getDefault().register(this);
-            typeCol.setCellValueFactory(new PropertyValueFactory<>("parkingType"));
-            paymentCol.setCellValueFactory(new PropertyValueFactory<>("paymentMethod"));
-            pricesCol.setCellValueFactory(new PropertyValueFactory<>("price"));
-            noCol.setCellValueFactory(new PropertyValueFactory<>("numberOfCars"));
-            hopCol.setCellValueFactory(new PropertyValueFactory<>("hoursOfParking"));
-
-            PricesMessage msg = new PricesMessage(Message.MessageType.REQUEST, PricesMessage.RequestType.GET_PRICES_TABLE);
-            Manager current_manager = (Manager) SimpleClient.getCurrent_user();
-            parking_lot.setText(current_manager.getParkinglot().getName());
-            msg.parkingLot =  current_manager.getParkinglot().getName();
-            SimpleClient.getClient().sendToServer(msg);
-        }
-
-
-        //initialize prices table from server
-        @Subscribe
-        public void newResponse(PricesMessage new_message) {
-            switch (new_message.response_type) {
-                case SET_PRICES_TABLE:
-                    pricesTable.setItems(FXCollections.observableArrayList(new_message.priceList));
-                    break;
-                case WAITING_FOR_APPROVAL:
-                    sendAlert("Price change request have been sent."+ "\n" +"Waiting For CEO To Approve.",
-                            "Request Sent", Alert.AlertType.INFORMATION);
-                    break;
-            }
-        }
-
-
     }
 }
